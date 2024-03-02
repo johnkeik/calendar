1f8149d8d5c9a2aa71cfe4c8957e1bc4d88f803d
Yiannis Keikoglou <yiannis.keikoglou@foodtecsolutions.com>
2024-03-02 13:01:06 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarComponentModule } from './calendar/calendar.module';
+import { ModalService } from './components/modal/modal.service';
+import { ModalContentComponent } from './components/modal-content/modal-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarComponentModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'calendar';
+
+  @ViewChild('view', { static: true, read: ViewContainerRef })
+  vcr!: ViewContainerRef;
+
+  constructor(private modalService: ModalService) {}
+
+  openModalTemplate(view: TemplateRef<Element>) {
+    this.modalService.open(this.vcr, view, {
+      animations: {
+        modal: {
+          enter: 'enter-slide-down 0.8s',
+        },
+        overlay: {
+          enter: 'fade-in 0.8s',
+          leave: 'fade-out 0.3s forwards',
+        },
+      },
+      size: {
+        width: '40rem',
+      },
+    });
+  }
+
+  openModalComponent() {
+    this.modalService.open(ModalContentComponent, {
+      animations: {
+        modal: {
+          enter: 'enter-scaling 0.3s ease-out',
+          leave: 'fade-out 0.3s forwards',
+        },
+        overlay: {
+          enter: 'fade-in 1s',
+          leave: 'fade-out 0.3s forwards',
+        },
+      },
+      size: {
+        width: '40rem',
+      },
+    });
+  }
+
+  close() {
+    this.modalService.close();
+  }
 }
---
+++ src/app/calendar/calendar.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarComponent } from './calendar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
+  declarations: [CalendarComponent],
+  exports: [CalendarComponent],
+})
+export class CalendarComponentModule {}
